b9a5c196c8301bf4031e83fc57e7d341fe2e6893
nayema34 <jannatnayema2@gmail.com>
2026-07-22 01:35:41 +0600

    refactor: enhance database connection logic with fallbacks and retry handling

--- config/db.js
+++ config/db.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 
+const DEFAULT_URI = 'mongodb+srv://jannatnayema2_db_user:nayema123@nayema.wjzjamd.mongodb.net/?appName=Nayema';
+
 const connectDB = async () => {
+  const uri = process.env.MONGODB_URI || DEFAULT_URI;
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    const conn = await mongoose.connect(uri, {
       dbName: 'StudyNook',
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
